44387641ff015088c4b117f8f1558fd5790a9f2c
Alex <alx.bec@me.com>
2015-05-14 17:26:26 +0200

    Update rvsh.sh

--- rvsh.sh
+++ rvsh.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
+# Réseau virtuel de machines Linux
 
 if [ $1 = "-connect" ];then
   if [ $# = 3 ];then
     nom_machine = $2
     nom_utilisateur = $3
+  else echo "Préciser nom machine et nom utilisateur"  
   fi
 elif [ $1 = "-admin" ];then
-else echo "Il faut préciser l'option -connect ou -admin"
+else echo "Préciser l'option -connect ou -admin"
 fi
