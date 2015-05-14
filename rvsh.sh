c4d8728cfce8f447e4400e36d03b09bef3646a5f
Alex <alx.bec@me.com>
2015-05-14 20:07:47 +0200

    Update rvsh.sh

--- rvsh.sh
+++ rvsh.sh
@@ -1,29 +1,43 @@
 #! /bin/bash
 # Réseau virtuel de machines Linux
 
-function who {
-  
+function who{
+# Acces a l'ensemble des utilisateurs connectes sur une machine
+# Doit renvoyer nom/heure/date
+# Un meme utilisateur peut se connecter plusieurs fois sur une machine depuis diff. terminaux
 }
-function rusers {
-  
+function rusers{
+# Liste des utilisateurs connectés sur le réseau
+# Doit renvoyer nom/heure/date
 }
-function rhost {
-  
+function rhost{
+# Renvoit la liste des machines rattachées au réseau virtuel  
 }
-function connect {
-  
+function connect{
+# Se connecter a une machine du réseau
 }
-function su {
-  
+function su{
+# Changer d'utilisateur
 }
-function passwd {
-  
+function passwd{
+# Changement de mot de passe sur l'ensemble du réseau virtuel
 }
-function finger {
-  
+function finger{
+# Renvoit des éléments complémentaires sur l'utilisateur  
 }
-function write {
-  
+function write{
+# Envoyer un message à un utilisateur connecté sur une machine du réseau
+# write nom_utilisateur@nom_machine message
+}
+
+function host{
+# Admin ajoute/enlève machine au réseau  
+}
+function users{
+# Admin ajoute/enlève utilisateur/droits/mdp
+}
+function afinger{
+# Admin renseigne sur un utilisateur, accès avec finger
 }
 
 if [ $1 = "-connect" ];then
