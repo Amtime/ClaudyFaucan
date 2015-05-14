03e5cb4049cb96bc83716cce94e3236dc8eff116
Alex <alx.bec@me.com>
2015-05-14 17:12:18 +0200

    Update rvsh.sh

--- rvsh.sh
+++ rvsh.sh
@@ -1,3 +1,10 @@
 #! /bin/bash
 
-if{}
+if [ $1 = "-connect" ];then
+  if [ $# = 3 ];then
+    nom_machine = $2
+    nom_utilisateur = $3
+  fi
+elif [ $1 = "-admin" ];then
+else echo "Il faut préciser l'option -connect ou -admin"
+fi
