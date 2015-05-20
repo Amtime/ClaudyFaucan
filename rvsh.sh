536ce25bfbaae579e4a3ec0a476ecf64f3de46d7
Amtime <kilian.favero@hotmail.fr>
2015-05-20 20:37:04 +0200

    Correction, c'que j'ai écris à l'air de fonctionner, reste le problème de la cla connexion

--- rvsh.sh
+++ rvsh.sh
@@ -52,30 +52,46 @@ function afinger {
 }
 
 function addcmd {
+# Permet d'ajouter les librairies et les fichiers au bon fonctionnement des commandes passées en argument et qui
+# aura l'adresse de la racine du fichier utilisateur en premier paramètre
+  local locadress=$1
+  shift 
+  echo $*
   for cmd in $@
   do
+# On trouve toutes les adresses de la commande grâce à whereis    
     adresscmd=$(whereis -b $cmd|cut -f2 -d ':')
-    echo $adress
+    echo "Installation des commandes : $*"
     for j in $adresscmd
     do
-      echo "$j $ADRESS$j"
-      mkdir -p $ADRESS$j
-      cp -r $j $ADRESS$j
-      for k in `ldd /usr/bin/ssh|cut -f2 -d '>'|cut -f1 -d '('`
-      do
-        mkdir -p $ADRESS$k
-        cp -R -L $k $ADRESS$j
-      done
+# On crée les répertoires et copie leurs contenus        
+      echo "Installation du répertoire : $j à l'adresse $locadress$j"
+      mkdir -p $locadress$j
+      cp -r $j $locadress$j
+    done
+    for k in `ldd /usr/bin/ssh|cut -f2 -d '>'|cut -f1 -d '('`
+    do
+# On s'occupe maintenant des bibliothèques nécessaires grâce à la commande ldd et procède de la même façon        
+      echo "Installation de la librairie $k à l'adresse $locadress$k"
+      mkdir -p $locadress$k
+      cp -R -L $k $locadress$j
     done
   done
 }
 
 function newuser {
-  if [ -d "$ADRESS" ];then
+# Fonction qui permet de créer un nouvel utilisateur donc l'adresse de la racine sera passée en paramètre
+  if [ -d "$1" ];then
     echo "L'utilisateur éxiste déjà"
+    exit 1
   fi
-  mkdir -p $ADRESS/bin $ADRESS/usr 
-  addcmd ssh
+  locadress=$1
+  echo $locadress
+  echo Salut
+# On crée ce qui sera la racine du dossier de l'utilisateur virtualisé
+  mkdir -p $1/bin $1/usr 
+  addcmd $locadress ssh
+  echo "Création du nouvel utilisateur finie"
 }
 
 
@@ -89,7 +105,7 @@ if [ "$1" = "-connect" ];then
     MACHINE=$2
     USER=$3
     ADRESS=/srv/$USER
-    newuser
+    newuser $ADRESS
   else 
     echo "Préciser nom machine et nom utilisateur"  
   fi
