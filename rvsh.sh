94a2ce8d13025ef0f0f2683597ed710a700010c0
Amtime <kilian.favero@hotmail.fr>
2015-05-20 15:43:30 +0200

    Début de la création d'utilisateur et la fonction qui ajoute les commandes à celui ci

--- rvsh.sh
+++ rvsh.sh
@@ -1,51 +1,89 @@
 #! /bin/bash
 # Réseau virtuel de machines Linux
 
-function who{
+function who {
 # Acces a l'ensemble des utilisateurs connectes sur une machine
 # Doit renvoyer nom/heure/date
 # Un meme utilisateur peut se connecter plusieurs fois sur une machine depuis diff. terminaux
+    echo 1
 }
-function rusers{
+function rusers {
 # Liste des utilisateurs connectés sur le réseau
 # Doit renvoyer nom/heure/date
+    echo 1
 }
-function rhost{
+function rhost {
 # Renvoit la liste des machines rattachées au réseau virtuel  
+    echo 1
 }
-function connect{
+function connect {
 # Se connecter a une machine du réseau
+    echo 1
 }
-function su{
+function su {
 # Changer d'utilisateur
+    echo 1
 }
-function passwd{
+function passwd {
 # Changement de mot de passe sur l'ensemble du réseau virtuel
+    echo 1
 }
-function finger{
+function finger {
 # Renvoit des éléments complémentaires sur l'utilisateur  
+    echo 1
 }
-function write{
+function write {
 # Envoyer un message à un utilisateur connecté sur une machine du réseau
 # write nom_utilisateur@nom_machine message
+    echo 1
 }
 
-function host{
+function host {
 # Admin ajoute/enlève machine au réseau  
+    echo 1
 }
-function users{
+function users {
 # Admin ajoute/enlève utilisateur/droits/mdp
+    echo 1
 }
-function afinger{
+function afinger {
 # Admin renseigne sur un utilisateur, accès avec finger
+    echo 1
 }
 
-if [ $1 = "-connect" ];then
-  if [ $# = 3 ];then
-    nom_machine = $2
-    nom_utilisateur = $3
-  else echo "Préciser nom machine et nom utilisateur"  
+function addcmd {
+  for cmd in $@
+  do
+    chemin=$(whereis -b $cmd|cut -f2 -d ' ')
+    cp -r $chemin $ADRESS$chemin
+  done
+}
+
+function newuser {
+  if [ -d "$ADRESS" ];then
+    echo "L'utilisateur éxiste déjà"
   fi
-elif [ $1 = "-admin" ];then
-else echo "Préciser l'option -connect ou -admin"
+  mkdir -p $ADRESS/bin $ADRESS/usr 
+  addcmd ls
+}
+
+
+if [ "`id -u`" != "0" ];then
+  echo "Nous avons besoin d'être en root."
+  exit 1
+fi
+
+if [ "$1" = "-connect" ];then
+  if [ "$#" = "3" ];then
+    MACHINE=$2
+    USER=$3
+    ADRESS=/srv/$USER
+    newuser
+  else 
+    echo "Préciser nom machine et nom utilisateur"  
+  fi
+  elif [ "$1" = "-admin" ];then
+      echo "admin"
+  else
+    echo "Préciser l'option -connect ou -admin"
 fi
