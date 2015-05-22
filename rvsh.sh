965eb9603e17160439e8189e5c1d24da08a772fa
Amtime <kilian.favero@hotmail.fr>
2015-05-22 17:15:30 +0200

    Création d'une session virtualisée + gestion des logs

--- rvsh.sh
+++ rvsh.sh
@@ -51,20 +51,82 @@ function afinger {
     echo 1
 }
 
-if [ "`id -u`" != "0" ];then
-  echo "Nous avons besoin d'être en root."
-  exit 1
+function log {
+
+# Permet la création d'une nouvelle ligne de log
+
+  local machine=$1 
+  local user=$2
+  local heure=`date|cut -f2 -d ','|cut -f1 -d '('|sed 's/ //g'`
+  local date=`date|cut -f1 -d ','|sed 's/\(.*\) \(.*\) \(.*\) \(.*\)/\2 \3 \4/'`
+  echo "$machine $user $date $heure connecté">>log
+}
+
+function virtualisation {
+
+# Permet la création d'un prompt, cette fonction reçoit la 
+# machine puis le nom d'utilisateur en paramètre. Elle met
+# aussi les logs à jour ou crée une nouvelle ligne de log
+# si nécessaire.
+
+  local cmd=null
+  local machine=$1
+  local user=$2
+  local heure=`date|cut -f2 -d ','|cut -f1 -d '('|sed 's/ //'`
+
+# Gestion des logs
+
+  if [ -z  "`cat 'log'|grep -e $machine -e $user`" ];then
+    echo "Création d'un new log"
+    log machine user
+  else
+    echo "Actualisation des logs"
+    sed -i "s/\($machine $user .*\)..:..:.*/\1 ${heure}connecté/" log
+    echo `cat 'log'`
+  fi
+  
+# Gestion du prompt
+
+  while [ "$cmd" != "exit" ]
+  do
+    echo "$2@$1 >"
+    read cmd
+  done
+
+# Mis à jour des logs avant de quitter la session :
+# Passage de l'état connecté à l'état déconnecté
+
+  sed -i "s/\(machine user .* \)connecté/\1déconnecté/g" log
+}
+
+function admin {
+  local cmd=null
+
+  while [ "$cmd" != "exit" ]
+  do
+    echo "rvsh >"
+    read cmd
+  done
+}
+
+# DEBUT DU SCRIPT
+
+# Création du fichie log si ce dernier n'éxiste pas
+
+if [ ! -w log ];then
+    echo "Création du fichier log"
 fi
 
 if [ "$1" = "-connect" ];then
   if [ "$#" = "3" ];then
     MACHINE=$2
     USER=$3
+    virtualisation $MACHINE $USER
   else 
     echo "Préciser nom machine et nom utilisateur"  
   fi
   elif [ "$1" = "-admin" ];then
-      echo "admin"
+      admin
   else
     echo "Préciser l'option -connect ou -admin"
 fi
