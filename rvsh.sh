95f0c7bc97e14f9c2bee1511434b8798f71aa7fa
Alex <alx.bec@me.com>
2015-06-13 16:21:22 +0200

    Update rvsh.sh

--- rvsh.sh
+++ rvsh.sh
@@ -95,7 +95,7 @@ function write {
 # On vérifie que l'utilisateur existe dans la base de donnée  
   if [ -n "`grep "^$nom_utilisateur:" passwd`" ]; then
     echo "Utilisateur existe"
-    if [ -n "$(echo "`awk "/^.* $nom_utilisateur .* connecté$/{print $1}"`")" ]; then
+    if [ -n "$(echo "`awk "/^.* $nom_utilisateur .* connecté$/{print $1}" log`")" ]; then
      echo "zizi"
     else
       echo "L'utilisateur n'est pas connecté"
